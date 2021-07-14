88c7d6d27e46c87b184574d88536632c3f6fe4c4
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-07-14 11:40:28 -0400

    Add counter using a complex useReducer approach with objects

--- src/components/CounterOne.js
+++ src/components/CounterOne.js
@@ -1,32 +1,60 @@
-import React, { useReducer } from 'react';
+import React, { useReducer } from "react";
 
-const initialState = 0;
+const initialState = {
+  firstCounter: 0,
+};
 
 const reducer = (state, action) => {
-
-    switch (action)
-    {
-        case 'increment':
-            return state + 1;
-        case 'decrement':
-            return state - 1;
-        case 'reset':
-            return initialState;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "increment":
+      return {
+        firstCounter: state.firstCounter + 1,
+      };
+    case "decrement":
+      return {
+        firstCounter: state.firstCounter - 1,
+      };
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 const CounterOne = () => {
-    const [count, dispatch] = useReducer(reducer, initialState);
-    return (
-        <div>
-            <div>Count - {count}</div>
-            <button onClick={() => {dispatch('increment')}}>Increment</button>
-            <button onClick={() => {dispatch('decrement')}}>Decrement</button>
-            <button onClick={() => {dispatch('reset')}}>Reset</button>
-        </div>
-    )
-}
+  const [count, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div>
+      <div>Count - {count.firstCounter}</div>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "increment"
+          });
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "decrement"
+          });
+        }}
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "reset"
+          });
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
 
 export default CounterOne;
