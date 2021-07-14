21c2c98fb3ab47d0964128c25a6a421facfd1442
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-07-14 16:18:15 -0400

    Add counter with useState

--- src/components/CounterOne.js
+++ src/components/CounterOne.js
@@ -1,30 +1,15 @@
-import React, { useReducer } from 'react';
+import React, { useState } from 'react';
 
 const initialState = 0;
 
-const reducer = (state, action) => {
-
-    switch (action)
-    {
-        case 'increment':
-            return state + 1;
-        case 'decrement':
-            return state - 1;
-        case 'reset':
-            return initialState;
-        default:
-            return state;
-    }
-}
-
 const CounterOne = () => {
-    const [count, dispatch] = useReducer(reducer, initialState);
+    const [count, setCount] = useState(initialState);
     return (
         <div>
             <div>Count - {count}</div>
-            <button onClick={() => {dispatch('increment')}}>Increment</button>
-            <button onClick={() => {dispatch('decrement')}}>Decrement</button>
-            <button onClick={() => {dispatch('reset')}}>Reset</button>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <button onClick={() => setCount(initialState)}>Reset</button>
         </div>
     )
 }
