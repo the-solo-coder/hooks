4c2ed37578409da4ff6113643fd1c130596f564f
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-07-14 16:30:47 -0400

    Add counters with multiple useState

--- src/components/CounterOne.js
+++ src/components/CounterOne.js
@@ -1,17 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const initialState = 0;
+const initialState1 = 0;
+const initialState2 = 10;
 
 const CounterOne = () => {
-    const [count, setCount] = useState(initialState);
-    return (
-        <div>
-            <div>Count - {count}</div>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-            <button onClick={() => setCount(count - 1)}>Decrement</button>
-            <button onClick={() => setCount(initialState)}>Reset</button>
-        </div>
-    )
-}
+  const [count1, setCount1] = useState(initialState1);
+  const [count2, setCount2] = useState(initialState2);
+  return (
+    <div>
+      <div>Counter 1 - {count1}</div>
+      <div>Counter 2 - {count2}</div>
+      <button onClick={() => setCount1(count1 + 1)}>Increment</button>
+      <button onClick={() => setCount1(count1 - 1)}>Decrement</button>
+      <button onClick={() => setCount1(count1 + 5)}>Increment 5</button>
+      <button onClick={() => setCount1(count1 - 5)}>Decrement 5</button>
+      <button
+        onClick={() => {
+          setCount1(initialState1);
+          setCount2(initialState2);
+        }}
+      >
+        Reset
+      </button>
+      <div>
+        <button onClick={() => setCount2(count2 + 1)}>
+          Increment Counter 2
+        </button>
+        <button onClick={() => setCount2(count2 - 1)}>
+          Increment Counter 2
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default CounterOne;
