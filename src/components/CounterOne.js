aac558bed6cd94d1e45b71bf8e7fa1bb324324a8
Aderson Oliveira <aolive24@my.centennialcollege.ca>
2021-07-14 15:39:20 -0400

    Add counter w/ useReducer and state properties

--- src/components/CounterOne.js
+++ src/components/CounterOne.js
@@ -2,17 +2,30 @@ import React, { useReducer } from "react";
 
 const initialState = {
   firstCounter: 0,
+  secondCounter: 10,
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "increment":
       return {
+        ...state,
         firstCounter: state.firstCounter + action.value,
       };
     case "decrement":
       return {
+        ...state,
         firstCounter: state.firstCounter - action.value,
+      };
+    case "increment2":
+      return {
+        ...state,
+        secondCounter: state.secondCounter + action.value,
+      };
+    case "decrement2":
+      return {
+        ...state,
+        secondCounter: state.secondCounter - action.value,
       };
     case "reset":
       return initialState;
@@ -25,12 +38,13 @@ const CounterOne = () => {
   const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
-      <div>Count - {count.firstCounter}</div>
+      <div>First Counter - {count.firstCounter}</div>
+      <div>Second Counter - {count.secondCounter}</div>
       <button
         onClick={() => {
           dispatch({
             type: "increment",
-            value: 1
+            value: 1,
           });
         }}
       >
@@ -40,7 +54,7 @@ const CounterOne = () => {
         onClick={() => {
           dispatch({
             type: "decrement",
-            value: 1
+            value: 1,
           });
         }}
       >
@@ -51,7 +65,7 @@ const CounterOne = () => {
         onClick={() => {
           dispatch({
             type: "increment",
-            value: 5
+            value: 5,
           });
         }}
       >
@@ -61,7 +75,7 @@ const CounterOne = () => {
         onClick={() => {
           dispatch({
             type: "decrement",
-            value: 5
+            value: 5,
           });
         }}
       >
@@ -71,12 +85,34 @@ const CounterOne = () => {
       <button
         onClick={() => {
           dispatch({
-            type: "reset"
+            type: "reset",
           });
         }}
       >
         Reset
       </button>
+      <div>
+        <button
+          onClick={() => {
+            dispatch({
+              type: "increment2",
+              value: 1,
+            });
+          }}
+        >
+          Increment Counter 2
+        </button>
+        <button
+          onClick={() => {
+            dispatch({
+              type: "decrement2",
+              value: 1,
+            });
+          }}
+        >
+          Decrement Counter 2
+        </button>
+      </div>
     </div>
   );
 };
